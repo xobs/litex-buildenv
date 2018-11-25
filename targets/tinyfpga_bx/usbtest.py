7ace1e974b4d77bad4d2530c682e395c9102425a
Tim 'mithro' Ansell <me@mith.ro>
2018-11-25 13:51:07 -0800

    Temp commit.

--- targets/tinyfpga_bx/usbtest.py
+++ targets/tinyfpga_bx/usbtest.py
@@ -2,6 +2,7 @@ from migen import *
 
 from litex.soc.cores import uart
 from litex.soc.cores.uart import UARTWishboneBridge
+from litex.soc.integration.soc_core import mem_decoder
 
 from litescope import LiteScopeAnalyzer
 from litescope import LiteScopeIO
@@ -16,8 +17,19 @@ class USBTestSoC(BaseSoC):
     csr_peripherals = (
         "analyzer",
         "io",
+        "usb",
     )
     csr_map_update(BaseSoC.csr_map, csr_peripherals)
+
+    interrupt_map = {
+        "usb": 3,
+    }
+    interrupt_map.update(BaseSoC.interrupt_map)
+
+    mem_map = {
+        "usb": 0x40000000,  # (default shadow @0xa0000000)
+    }
+    mem_map.update(BaseSoC.mem_map)
 
     def __init__(self, platform, *args, **kwargs):
 
@@ -31,24 +43,34 @@ class USBTestSoC(BaseSoC):
 
         # ValentyUSB
         usb_pads = platform.request("usb")
-        usb_device = usbcore.UsbDevice(usb_pads.d_p, usb_pads.d_n, 0)
-        self.submodules += ClockDomainsRenamer("usb_48")(usb_device)
+        usb_iobuf = usbcore.UsbIoBuf(usb_pads.d_p, usb_pads.d_n)
+        self.submodules.usb = usbcore.UsbDeviceCpuInterface(usb_iobuf)
+
+        self.add_wb_slave(mem_decoder(self.mem_map["usb"]), self.usb.bus)
+        self.add_memory_region(
+            "usb", self.mem_map["usb"] | self.shadow_base, 512)
 
         # Litescope for analyzing the BIST output
         # --------------------
+        led = Signal()
         self.submodules.io = LiteScopeIO(8)
-        self.comb += platform.request("user_led", 0).eq(self.io.output[0])
+        self.comb += [
+            led.eq(self.io.output[0]),
+            platform.request("user_led", 0).eq(led),
+        ]
         # Give litescope control over the pullup.
         self.comb += usb_pads.pullup.eq(self.io.output[1])
 
         analyzer_signals = [
-            usb_device.usb_tx_en,
-            usb_device.usb_p_tx,
-            usb_device.usb_n_tx,
-            usb_device.usb_p_rx,
-            usb_device.usb_n_rx,
+            led,
+            #usb_pads.pullup,
+            #usb_device.usb_tx_en,
+            #usb_device.usb_p_tx,
+            #usb_device.usb_n_tx,
+            #usb_device.usb_p_rx,
+            #usb_device.usb_n_rx,
         ]
-        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 128, clock_domain="usb_48")
+        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 1024) #, clock_domain="usb_48")
 
     def do_exit(self, vns, filename="test/analyzer.csv"):
         self.analyzer.export_csv(vns, filename)
