b5e394c3bd16e61a38995e3d79c3c8cae9f12cba
Sean Cross <sean@xobs.io>
2019-02-11 12:47:58 +0800

    download-env.sh: correct "newlib" note
    Quick change to swap the lm32 and riscv newlib notes.

--- download-env.sh
+++ download-env.sh
@@ -4,12 +4,12 @@ if [ "x$1" = "xlm32" ]
 then
     PLATFORM=fomu_evt CPU_VARIANT=minimal TARGET=usb FIRMWARE=tinyusb ./scripts/download-env.sh
     echo "Run this once you enter the env:"
-    echo "conda install gcc-riscv32-elf-newlib"
+    echo "conda install gcc-lm32-elf-newlib"
 elif [ "x$1" = "xvexriscv" ]
 then
     PLATFORM=fomu_evt CPU_VARIANT=min CPU=vexriscv TARGET=usb FIRMWARE=tinyusb ./scripts/download-env.sh
     echo "Run this once you enter the env:"
-    echo "conda install gcc-lm32-elf-newlib"
+    echo "conda install gcc-riscv32-elf-newlib"
 else
     echo "No CPU specified"
     echo "Usage: $0 [lm32|vexriscv]"
